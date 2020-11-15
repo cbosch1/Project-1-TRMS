66445cc53c84555764b85b3bd439903941357173
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-15 15:03:14 -0500

    Created test structures for applicable classes

---
+++ src/test/java/TRMS/services/UserServiceTest.java
@@ -0,0 +1,36 @@
+package TRMS.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+            
+    @BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
