8b2992859b188379cfd6628bfe47e56039ef62e2
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-15 22:06:29 -0500

    Set up environment for tests

--- src/test/java/TRMS/daos/AttachmentDaoTest.java
+++ src/test/java/TRMS/daos/AttachmentDaoTest.java
@@ -1,6 +1,13 @@
 package TRMS.daos;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,10 +15,29 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import TRMS.pojos.Attachment;
+import TRMS.util.ConnectionUtil;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AttachmentDaoTest {
+
+	@Mock
+	private ConnectionUtil connUtil;
+	@Mock
+	private Connection connection;
+
+	private Connection realConn;
+	private PreparedStatement stmt;
+	private PreparedStatement testStmt;
+	private PreparedStatement spy;
+
+	private AttachmentDaoPostgres attachDao;
+	private Attachment attach;
+
     
     @BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -23,10 +49,17 @@ public class AttachmentDaoTest {
 
 	@Before
 	public void setUp() throws Exception {
+		realConn = new ConnectionUtil().createConnection();
+		attachDao = new AttachmentDaoPostgres(connUtil);
+		attach = new Attachment(2010), 2010, "College Transcripts");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (stmt != null){
+			stmt.close();
+		}
+		realConn.close();
 	}
 
 	@Test
@@ -47,5 +80,19 @@ public class AttachmentDaoTest {
 	@Test
 	public void deleteAttachmentTest() {
 		fail("Not yet implemented");
+	}
+
+	/**
+	 * Helper method that initializes mock and spy variables using the prepared sql string provided
+	 * @param sql -Prepared SQL String
+	 */
+	private void initStmtHelper(String sql) throws SQLException {
+		//Prep Mockito Spy
+		stmt = realConn.prepareStatement(sql);
+		spy = Mockito.spy(stmt);
+
+		//Set standard connection mocking methods
+		when(connUtil.createConnection()).thenReturn(connection);
+		when(connection.prepareStatement(sql)).thenReturn(spy);
 	}
 }
