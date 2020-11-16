92e238a7b2b9788cc8be774c2f759abbfe1dc9c1
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-15 22:53:00 -0500

    Set up testing environment

--- src/test/java/TRMS/daos/InfoRequestDaoTest.java
+++ src/test/java/TRMS/daos/InfoRequestDaoTest.java
@@ -1,6 +1,14 @@
 package TRMS.daos;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,11 +16,29 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import TRMS.util.ConnectionUtil;
+import TRMS.pojos.InfoRequest;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InfoRequestDaoTest {
-        
+
+	@Mock
+	private ConnectionUtil connUtil;
+	@Mock
+	private Connection connection;
+
+	private Connection realConn;
+	private PreparedStatement stmt;
+	private PreparedStatement testStmt;
+	private PreparedStatement spy;
+
+	private InfoRequestDao infoDao;
+	private InfoRequest info;
+
     @BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -23,10 +49,17 @@ public class InfoRequestDaoTest {
 
 	@Before
 	public void setUp() throws Exception {
+		realConn = new ConnectionUtil().createConnection();
+		infoDao = new InfoRequestDaoPostgres(connUtil);
+		info = new InfoRequest(2010, 2010, 2010, true, "Urgent request for information needs approval!", LocalDateTime.of(2020, 11, 15, 15, 30));
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (stmt != null){
+			stmt.close();
+		}
+		realConn.close();
 	}
 
 	@Test
@@ -57,5 +90,19 @@ public class InfoRequestDaoTest {
 	@Test
 	public void deleteInfoRequestTest() {
 		fail("Not yet implemented");
+	}
+
+	/**
+	 * Helper method that initializes mock and spy variables using the prepared sql string provided
+	 * @param sql -Prepared SQL String
+	 */
+	private void initStmtHelper(String sql) throws SQLException {
+		//Prep Mockito Spy
+		stmt = realConn.prepareStatement(sql);
+		spy = Mockito.spy(stmt);
+
+		//Set standard connection mocking methods
+		when(connUtil.createConnection()).thenReturn(connection);
+		when(connection.prepareStatement(sql)).thenReturn(spy);
 	}
 }
