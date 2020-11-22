c1cfb74c415e2dc0aee544dc9ba521456ff57068
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-22 13:47:08 -0500

    Implemented basic tests

--- src/test/java/TRMS/controllers/UserControlTest.java
+++ src/test/java/TRMS/controllers/UserControlTest.java
@@ -1,6 +1,8 @@
 package TRMS.controllers;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,10 +10,24 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import TRMS.enums.AuthPriv;
+import TRMS.pojos.User;
+import TRMS.services.UserService;
+import io.javalin.http.Context;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControlTest {
+
+	@Mock
+	private UserService mockService;
+	@Mock
+	private Context mockCtx;
+
+	private UserControl controlToTest;
+	private User user;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -23,6 +39,14 @@ public class UserControlTest {
 
 	@Before
 	public void setUp() throws Exception {
+		controlToTest = new UserControl(mockService);
+		user = new User(2010, "username", "hashedpassword123467890", 0, AuthPriv.EMPLOYEE);
+
+		when(mockCtx.formParam("userId")).thenReturn(Integer.toString(user.getUserId()));
+		when(mockCtx.formParam("username")).thenReturn(user.getUsername());
+		when(mockCtx.formParam("password")).thenReturn(user.getPassword());
+		when(mockCtx.formParam("employeeId")).thenReturn(Integer.toString(user.getEmployeeId()));
+		when(mockCtx.formParam("privilege")).thenReturn(user.getPrivilege().toString());
 	}
 
 	@After
@@ -31,21 +55,79 @@ public class UserControlTest {
 
 	@Test
 	public void createUserTest() {
-		fail("Not yet implemented");
+		try {
+			controlToTest.createUser(mockCtx);
+
+			verify(mockCtx).formParam("username");
+			verify(mockCtx).formParam("password");
+			verify(mockCtx).formParam("employeeId");
+			verify(mockCtx).formParam("privilege");
+
+			verify(mockService).createUser(user.getUsername(), user.getPassword(), user.getEmployeeId(), user.getPrivilege());
+
+			//TODO verify ctx being given proper inputs.
+			
+			verify(mockCtx).status(200);
+
+		} catch (Exception e) {
+			fail("Exception thrown during create test: " + e);
+		}
 	}
 
 	@Test
 	public void readUserTest() {
-		fail("Not yet implemented");
+		try {
+			controlToTest.readUser(mockCtx);
+
+			verify(mockCtx).formParam("userId");
+			verify(mockService).readUser(user.getUserId());
+
+			//TODO verify ctx being given proper inputs.
+
+			verify(mockCtx).status(200);
+		
+		} catch (Exception e) {
+			fail("Exception thrown during read test: " + e);
+		}
 	}
 
 	@Test
 	public void updateUserTest() {
-		fail("Not yet implemented");
+		try {
+			controlToTest.updateUser(mockCtx);
+	
+			verify(mockCtx).formParam("userId");
+			verify(mockCtx).formParam("username");
+			verify(mockCtx).formParam("password");
+			verify(mockCtx).formParam("employeeId");
+			verify(mockCtx).formParam("privilege");
+	
+			verify(mockService).updateUser(user.getUserId(), user.getUsername(), user.getPassword(), 
+											user.getEmployeeId(), user.getPrivilege());
+			
+			//TODO verify ctx being given proper inputs.
+
+			verify(mockCtx).status(200);
+	
+		} catch (Exception e) {
+			fail("Exception thrown during update test: " + e);
+		}
 	}
 	
 	@Test
 	public void deleteUserTest() {
-		fail("Not yet implemented");
+		try {
+			controlToTest.deleteUser(mockCtx);
+
+			verify(mockCtx).formParam("userId");
+			verify(mockService).deleteUser(user.getUserId());
+
+			//TODO verify ctx being given proper inputs.
+
+			verify(mockCtx).status(200);
+		
+		} catch (Exception e) {
+			fail("Exception thrown during delete test: " + e);
+		}
 	}
 }
