daf2f031e6f7f8090439118b1ef7b54695e2dd2f
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-22 19:41:44 -0500

    Implemented methods to pass basic test cases

--- src/main/java/TRMS/Daos/ReimburseDaoPostgres.java
+++ src/main/java/TRMS/Daos/ReimburseDaoPostgres.java
@@ -1,12 +1,21 @@
 package TRMS.daos;
 
+import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import TRMS.enums.*;
 import TRMS.pojos.ReimburseRequest;
 import TRMS.util.ConnectionUtil;
 
@@ -17,6 +26,7 @@ import TRMS.util.ConnectionUtil;
 public class ReimburseDaoPostgres implements ReimburseRequestDao {
 
     private PreparedStatement stmt;
+    private PreparedStatement stmt2;
 
     private static Logger Log = LogManager.getLogger("Dao");
 
@@ -35,8 +45,38 @@ public class ReimburseDaoPostgres implements ReimburseRequestDao {
      */
     @Override
     public int createRequest(ReimburseRequest request) throws SQLException {
-        // TODO Auto-generated method stub
-        return 0;
+        int result; 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to insert reimbursement for employee with id: " + request.getEmployeeId());
+
+            String sql = "SELECT insert_reimbursement(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            stmt = conn.prepareCall(sql);
+
+            stmt.setInt(1, request.getEmployeeId());
+            stmt.setString(2, request.getLocation());
+            stmt.setDouble(3, request.getCost());
+            stmt.setString(4, request.getType().toString());
+            stmt.setString(5, request.getDescription());
+            stmt.setString(6, request.getJustification());
+            stmt.setDouble(7, request.getProjected());
+            stmt.setBoolean(8, request.isUrgent());
+            stmt.setString(9, request.getStatus().toString());
+            stmt.setString(10, request.getStage().toString());
+            stmt.setDate(11, Date.valueOf(LocalDate.from(request.getDateTime())));
+            stmt.setTime(12, Time.valueOf(LocalTime.from(request.getDateTime())));
+            
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            result = rs.getInt(1);
+            Log.info("Request completed, generated reimbursement request id: " + result);
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in create reimbursement for employee with id: " + request.getEmployeeId(), e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -46,8 +86,29 @@ public class ReimburseDaoPostgres implements ReimburseRequestDao {
      */
     @Override
     public boolean deleteRequest(int requestId)  throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        boolean result = false; 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to delete reimbursement with id: " + requestId);
+
+            String sql = "DELETE FROM reimbursement WHERE request_id = ?;";
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, requestId);
+
+            result = (1 == stmt.executeUpdate());
+
+            if (result){
+                Log.info("Request completed, reimbursement with id: " + requestId + " was deleted.");
+            } else
+                Log.warn("Request to delete reimbursement with id: " + requestId +" was NOT completed");
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in delete for reimbursement id: " + requestId, e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -56,8 +117,36 @@ public class ReimburseDaoPostgres implements ReimburseRequestDao {
      */
     @Override
     public List<ReimburseRequest> readAllRequests() throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        List<ReimburseRequest> result = new ArrayList<>(); 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to retrieve all reimbursement requests");
+
+            String sql = "SELECT * FROM reimbursement;";
+            String sql2 = "SELECT * FROM reimburse_status;";
+            stmt = conn.prepareStatement(sql);
+            stmt2 = conn.prepareStatement(sql2);
+
+            ResultSet rs = stmt.executeQuery();
+            ResultSet rs2 = stmt2.executeQuery();
+
+            while (rs.next()){
+                rs2.next();
+                ReimburseRequest r = new ReimburseRequest(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getDouble(4),
+                                    EventType.valueOf(rs.getString(5)), rs.getString(6), rs.getString(7), rs2.getDouble(2),
+                                    rs2.getBoolean(3), AppStatus.valueOf(rs2.getString(4)), AppStage.valueOf(rs2.getString(5)),
+                                    LocalDateTime.of(rs2.getDate(6).toLocalDate(), rs2.getTime(7).toLocalTime()));                    
+                result.add(r);
+            }
+
+            Log.info("Request completed, retrieved reimbursement requests, count: " + result.size());
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in read all reimbursement requests", e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -68,8 +157,41 @@ public class ReimburseDaoPostgres implements ReimburseRequestDao {
      */
     @Override
     public List<ReimburseRequest> readAllRequestsFor(int employeeId) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        List<ReimburseRequest> result = new ArrayList<>(); 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to retrieve reimbursement requests related to employee with id: " + employeeId);
+
+            String sql = "SELECT * FROM reimbursement WHERE emp_id = ?;";
+            String sql2 = "SELECT * FROM reimburse_status WHERE request_id IN "
+                            + "(SELECT request_id FROM reimbursement WHERE emp_id = ?);";
+            stmt = conn.prepareStatement(sql);
+            stmt2 = conn.prepareStatement(sql2);
+
+            stmt.setInt(1, employeeId);
+            stmt2.setInt(1, employeeId);
+
+            ResultSet rs = stmt.executeQuery();
+            ResultSet rs2 = stmt2.executeQuery();
+
+            while (rs.next()){
+                rs2.next();
+                ReimburseRequest r = new ReimburseRequest(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getDouble(4),
+                                    EventType.valueOf(rs.getString(5)), rs.getString(6), rs.getString(7), rs2.getDouble(2),
+                                    rs2.getBoolean(3), AppStatus.valueOf(rs2.getString(4)), AppStage.valueOf(rs2.getString(5)),
+                                    LocalDateTime.of(rs2.getDate(6).toLocalDate(), rs2.getTime(7).toLocalTime()));                    
+                result.add(r);
+            }
+
+            Log.info("Request completed, retrieved reimbursement requests " 
+                    +"related to employee with id: " +employeeId+ ", count: " + result.size());
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in read all reimbursement requests related to employee with id: " + employeeId, e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -79,8 +201,40 @@ public class ReimburseDaoPostgres implements ReimburseRequestDao {
      */
     @Override
     public ReimburseRequest readRequest(int requestId) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        ReimburseRequest result = null; 
+        
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to retrieve reimbursement request with id: " + requestId);
+
+            conn.setAutoCommit(false);
+
+            String sql = "SELECT * FROM reimbursement WHERE request_id = ?;";
+            String sql2 = "SELECT * FROM reimburse_status WHERE request_id = ?;";
+            stmt = conn.prepareStatement(sql);
+            stmt2 = conn.prepareStatement(sql2);
+            stmt.setInt(1, requestId);
+            stmt2.setInt(1, requestId);
+
+            ResultSet rs = stmt.executeQuery();
+            ResultSet rs2 = stmt2.executeQuery();
+            conn.commit();
+
+            while (rs.next()){
+                rs2.next();
+                result = new ReimburseRequest(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getDouble(4),
+                                    EventType.valueOf(rs.getString(5)), rs.getString(6), rs.getString(7), rs2.getDouble(2),
+                                    rs2.getBoolean(3), AppStatus.valueOf(rs2.getString(4)), AppStage.valueOf(rs2.getString(5)),
+                                    LocalDateTime.of(rs2.getDate(6).toLocalDate(), rs2.getTime(7).toLocalTime()));
+            }
+
+            Log.info("Request completed, retrieved reimbursement request with id: " + requestId);
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in reimbursement request with id: " + requestId, e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -91,8 +245,49 @@ public class ReimburseDaoPostgres implements ReimburseRequestDao {
      */
     @Override
     public boolean updateRequest(ReimburseRequest request) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        boolean result = false; 
+        
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to update reimbursement request with id: " + request.getRequestId());
+
+            conn.setAutoCommit(false);
+
+            String sql = "UPDATE reimbursement SET ev_location = ?, ev_cost = ?, ev_type = ?::event_type, description = ?, "
+                                                +"justification = ? WHERE request_id = ?;";
+            String sql2 = "UPDATE reimburse_status SET projected_award = ?, urgent = ?, status = ?::app_status, "
+                                                +"stage = ?::app_stage, request_date = ?, request_time = ? WHERE request_id = ?;";
+            stmt = conn.prepareStatement(sql);
+            stmt2 = conn.prepareStatement(sql2);
+
+            stmt.setString(1, request.getLocation());
+            stmt.setDouble(2, request.getCost());
+            stmt.setString(3, request.getType().toString());
+            stmt.setString(4, request.getDescription());
+            stmt.setString(5, request.getJustification());
+            stmt.setInt(6, request.getRequestId());
+
+            stmt2.setDouble(1, request.getProjected());
+            stmt2.setBoolean(2, request.isUrgent());
+            stmt2.setString(3, request.getStatus().toString());
+            stmt2.setString(4, request.getStage().toString());
+            stmt2.setDate(5, Date.valueOf(LocalDate.from(request.getDateTime())));
+            stmt2.setTime(6, Time.valueOf(LocalTime.from(request.getDateTime())));
+            stmt2.setInt(7, request.getRequestId());
+
+            result = (1 == stmt.executeUpdate() && 1 == stmt2.executeUpdate());
+            conn.commit();
+
+            if (result){
+                Log.info("Request completed, reimbursement with id: " + request.getRequestId() + " was updated.");
+            } else
+                Log.warn("Request to update reimbursement with id: " + request.getRequestId() +" was NOT completed");
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in updating reimbursement request with id: " + request.getRequestId(), e);
+            throw e;
+        }
+
+        return result;
     }
     
 }
