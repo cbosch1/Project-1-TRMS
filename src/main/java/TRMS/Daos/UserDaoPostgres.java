21cbab746835abc02310e849b53ee9cafd03a947
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-22 19:41:53 -0500

    Implemented methods to pass basic test cases

--- src/main/java/TRMS/Daos/UserDaoPostgres.java
+++ src/main/java/TRMS/Daos/UserDaoPostgres.java
@@ -1,11 +1,14 @@
 package TRMS.daos;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import TRMS.enums.AuthPriv;
 import TRMS.pojos.User;
 import TRMS.util.ConnectionUtil;
 
@@ -33,8 +36,30 @@ public class UserDaoPostgres implements UserDao {
      */
     @Override
     public int createUser(User user) throws SQLException {
-        // TODO Auto-generated method stub
-        return 0;
+        int result; 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to insert user: " + user.getUsername());
+
+            String sql = "INSERT INTO userbase VALUES (Default,?,?,?,?::auth_priv) RETURNING user_id;";
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, user.getEmployeeId());
+            stmt.setString(2, user.getUsername());
+            stmt.setString(3, user.getPassword());
+            stmt.setString(4, user.getPrivilege().toString());
+
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            result = rs.getInt(1);
+            Log.info("Request completed, generated user id: " + result);
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in create related to user: " + user.getUsername(), e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -44,8 +69,29 @@ public class UserDaoPostgres implements UserDao {
      */
     @Override
     public boolean deleteUser(int userId) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        boolean result = false; 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to delete user with id: " + userId);
+
+            String sql = "DELETE FROM userbase WHERE user_id = ?;";
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, userId);
+
+            result = (1 == stmt.executeUpdate());
+
+            if (result){
+                Log.info("Request completed, user with id: " + userId + " was deleted.");
+            } else
+                Log.warn("Request to delete user with id: " + userId +" was NOT completed");
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in user delete for id: " + userId, e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -55,8 +101,28 @@ public class UserDaoPostgres implements UserDao {
      */
     @Override
     public User readUser(int userId) throws SQLException {
-        // TODO Auto-generated method stub
-        return null;
+        User result = null; 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to retrieve user with id: " + userId);
+
+            String sql = "SELECT * FROM userbase WHERE user_id = ?;";
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, userId);
+
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            result = new User(rs.getInt(1), rs.getString(3), rs.getString(4), rs.getInt(2), AuthPriv.valueOf(rs.getString(5)));
+
+            Log.info("Request completed, retrieved user: " + result.getUsername());
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in read for user with id: " + userId, e);
+            throw e;
+        }
+
+        return result;
     }
 
     /**
@@ -67,8 +133,33 @@ public class UserDaoPostgres implements UserDao {
      */
     @Override
     public boolean updateUser(User user) throws SQLException {
-        // TODO Auto-generated method stub
-        return false;
+        boolean result = false; 
+
+        try(Connection conn = connUtil.createConnection()) {
+            Log.info("Received request to update user with id: " + user.getUserId());
+
+            String sql = "UPDATE userbase SET emp_id = ?, username = ?, passphrase = ?, privilege = ?::auth_priv WHERE user_id = ?;";
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setInt(1, user.getEmployeeId());
+            stmt.setString(2, user.getUsername());
+            stmt.setString(3, user.getPassword());
+            stmt.setString(4, user.getPrivilege().toString());
+            stmt.setInt(5, user.getUserId());
+
+            result = (1 == stmt.executeUpdate());
+
+            if (result){
+                Log.info("Request completed, user with id: " + user.getUserId() + " was updated.");
+            } else
+                Log.warn("Request to update user with id: " + user.getUserId() +" was NOT completed");
+
+        } catch (SQLException e){
+            Log.warn("SQLException thrown in user update for id: " + user.getUserId(), e);
+            throw e;
+        }
+
+        return result;
     }
     
 }
