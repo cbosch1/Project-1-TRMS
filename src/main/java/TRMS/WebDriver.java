01a12d08d72eb35bd54aeb2938d5b2282c3a5ab5
Conner Bosch <cbosch1@mail.usf.edu>
2020-11-29 23:07:50 -0500

    update redirect

--- src/main/java/TRMS/WebDriver.java
+++ src/main/java/TRMS/WebDriver.java
@@ -118,7 +118,7 @@ public class WebDriver {
             app.put(MANAGER_URL+"/view-reimbursement/:id/:approval", ctx -> reimburseControl.reviewReimbursement(ctx));
             app.post(MANAGER_URL+"/view-reimbursement/:id/attachments", ctx -> { attachControl.readRelatedReferences(ctx);});
             app.post(MANAGER_URL+"/view-reimbursement/:id/information", ctx -> { infoControl.createInfoRequest(ctx);
-                                                                                 ctx.redirect("/portal");});
+                                                                                 ctx.redirect(MANAGER_URL + "/portal");});
             app.post(MANAGER_URL+"/view-reimbursement/:id/infos", ctx -> { infoControl.readAllInfoForManager(ctx);});    
             app.post(MANAGER_URL+"/view-info/:id", ctx -> { infoControl.readInfoRequest(ctx);});
             app.post(MANAGER_URL+"/myinfo", ctx -> { userControl.readUser(ctx);});
