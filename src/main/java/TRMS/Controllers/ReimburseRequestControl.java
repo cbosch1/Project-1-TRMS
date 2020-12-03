aef564e8b45bbd803f7d5849f49dd78f1086819c
Conner Bosch <cbosch1@mail.usf.edu>
2020-12-03 18:01:37 -0500

    Fixed hopefuly the last pathing issue

--- src/main/java/TRMS/Controllers/ReimburseRequestControl.java
+++ src/main/java/TRMS/Controllers/ReimburseRequestControl.java
@@ -308,7 +308,7 @@ public class ReimburseRequestControl {
             
             if (service.updateRequest(request)){
                 Log.info("Reimbursement request successfully updated");
-                ctx.res.sendRedirect("../../../");
+                ctx.res.sendRedirect("../../");
             } else {
                 Log.warn("Service returned false while updating reimbursement request");
                 ctx.status(500);
