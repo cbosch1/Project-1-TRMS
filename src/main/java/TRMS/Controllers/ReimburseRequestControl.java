a432d10011a6ebd58766237950afa0cc5fa95a78
Conner Bosch <cbosch1@mail.usf.edu>
2020-12-03 17:08:27 -0500

    Added redirect from payout update

--- src/main/java/TRMS/Controllers/ReimburseRequestControl.java
+++ src/main/java/TRMS/Controllers/ReimburseRequestControl.java
@@ -308,7 +308,7 @@ public class ReimburseRequestControl {
             
             if (service.updateRequest(request)){
                 Log.info("Reimbursement request successfully updated");
-                ctx.status(200);
+                ctx.redirect("../", 200);
             } else {
                 Log.warn("Service returned false while updating reimbursement request");
                 ctx.status(500);
